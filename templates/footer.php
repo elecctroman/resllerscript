7d4b204073aa629e487984b2fa9e05e7621c7aa8
elecctroman <55356201+elecctroman@users.noreply.github.com>
2025-09-28 18:41:15 +0300

    Delete templates directory

--- templates/footer.php
+++
@@ -1,9 +0,0 @@
-</main>
-<footer class="footer bg-dark text-white-50 py-3 mt-auto">
-    <div class="container text-center">
-        <small>© <?= date('Y') ?> Bayi Yönetim Sistemi</small>
-    </div>
-</footer>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
